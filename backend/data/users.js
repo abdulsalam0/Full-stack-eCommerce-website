955057722c48b9c473d096727e5b1459b09781c2
abdulsalam0 <a.aboubakar@lancaster.ac.uk>
2020-10-02 19:50:33 +0100

    add bcrypt/ created static user data

---
+++ backend/data/users.js
@@ -0,0 +1,22 @@
+import bcrypt from "bcryptjs";
+
+const user = [
+  {
+    name: "admin User",
+    email: "admin@ProProduct.com",
+    password: bcrypt.hashSync("123456", 10),
+    isAdmin: true,
+  },
+  {
+    name: "Abdalslam Aboubakar",
+    email: "abdul@ProProduct.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+  {
+    name: "test user",
+    email: "test@ProProduct.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+];
+
+export default user;
